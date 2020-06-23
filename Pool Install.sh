2a9e0abc89362cb7e1b8fdaaccbba048cd33beaf
rmanuka3 <58649522+rmanuka3@users.noreply.github.com>
2020-06-23 15:18:56 +0530

    Update Pool Install.sh

--- Pool Install.sh
+++ Pool Install.sh
@@ -1,3 +1,5 @@
-az vm extension set --resource-group AZP-002-RG --vm-name AZP-002-VM-0007 --name CustomScriptExtension --publisher Microsoft.Compute 
---settings '{"fileUris": ["https://dev.azure.com/ZFDevOpsPoc/AZP-002_TRATON/_git/AZP-002_DEVOPS?path=%2FScript_Azure_VM%2FAgent_Linux_Install.ps1"], 
-"commandToExecute":"powershell -ExecutionPolicy Unrestricted -File azagentconfig.ps1"}' --version 1.0
+#!/bin/sh
+cd /home/AzureUser;mkdir azagent;cd azagent;curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.170.1/vsts-agent-linux-x64-2.170.1.tar.gz;sleep 5;tar -zxvf vstsagent.tar.gz;sleep 10;
+sudo -E bin/Agent.Listener configure --unattended --nostart --replace --acceptteeeula --pool AZP-002_NXPTool --agent NXPTool  --url https://dev.azure.com/ZFDevOpsPoc/ --auth pat --token 55adylg6fwifrpk7e3ck7clbpzn3i7buh2pkof4xouv44bg4gboa
+sudo ./svc.sh install
+sudo ./svc.sh start
